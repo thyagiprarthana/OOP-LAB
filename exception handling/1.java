d39984647086896510bfa372f68c4baefd3f005c
thyagiprarthana <thyagiprarthana@gmail.com>
2025-04-05 22:16:23 +0530

    Create 1.java

---
+++ exception handling/1.java
@@ -0,0 +1,9 @@
+public class DivideByZero {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0; 
+       } catch (ArithmeticException e) {
+            System.out.println("Error: Cannot divide by zero.");
+        }
+    }
+}
