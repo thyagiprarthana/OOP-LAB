5237e09b1f85d14661c6a8027f0e2e34e39ecc35
thyagiprarthana <thyagiprarthana@gmail.com>
2025-04-05 22:11:13 +0530

    Create 1.java

---
+++ packages/1.java
@@ -0,0 +1,11 @@
+package conversion;
+
+public class TemperatureConverter {
+    public double celsiusToFahrenheit(double c) {
+        return (c * 9/5) + 32;
+    }
+
+    public double fahrenheitToCelsius(double f) {
+        return (f - 32) * 5/9;
+    }
+}
